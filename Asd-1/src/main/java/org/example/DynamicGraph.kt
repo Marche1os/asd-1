235c57949525eb1a404416dd59d359295a76bdc6
marche1os <kolpmark@yandex.ru>
2023-09-08 22:05:35 +0300

    ASD-1: made fixes to prepare graph to find bridges

--- Asd-1/src/main/java/org/example/DynamicGraph.kt
+++ Asd-1/src/main/java/org/example/DynamicGraph.kt
@@ -1,21 +1,21 @@
 package org.example
 
 class DynamicGraph<T> {
-    private val data = mutableSetOf<MutableSet<T>>()
+    private val components = mutableSetOf<MutableSet<Node<T>>>()
     private val edges = mutableSetOf<Pair<T, T>>()
 
     fun addEdge(vertex1: T, vertex2: T) {
         val (component1, component2) = find(vertex1) to find(vertex2)
 
         if (component1 == null && component2 == null) {
-            val newComponent = mutableSetOf(vertex1, vertex2)
-            data.add(newComponent)
+            val newComponent = mutableSetOf(Node(vertex1), Node(vertex2))
+            components.add(newComponent)
         } else if (component1 != null && component2 == null) {
-            component1.add(vertex2)
+            component1.add(vertex2.asNode())
         } else if (component1 == null && component2 != null) {
-            component2.add(vertex1)
+            component2.add(vertex1.asNode())
         } else if (component1 != null && component2 != null && component1 != component2) {
-            data.union(component1 + component2)
+            components.union(component1 + component2)
         }
 
         edges.add(vertex1 to vertex2)
@@ -26,11 +26,13 @@ class DynamicGraph<T> {
         if (edges.contains(edgeToRemove)) {
             edges.remove(edgeToRemove)
 
-            val actualComponents = mutableSetOf<MutableSet<T>>()
-            data.forEach { item ->
-                if (item.contains(vertex1) && item.contains(vertex2)) {
-                    item.remove(vertex1)
-                    item.remove(vertex2)
+            val actualComponents = mutableSetOf<MutableSet<Node<T>>>()
+            val nodeVertex1 = vertex1.asNode()
+            val nodeVertex2 = vertex2.asNode()
+            components.forEach { item ->
+                if (item.contains(nodeVertex1) && item.contains(nodeVertex2)) {
+                    item.remove(nodeVertex1)
+                    item.remove(nodeVertex2)
                     if (item.isNotEmpty()) {
                         actualComponents.add(item)
                     }
@@ -38,17 +40,37 @@ class DynamicGraph<T> {
                     actualComponents.add(item)
                 }
             }
-            data.clear()
-            data.addAll(actualComponents)
+            components.clear()
+            components.addAll(actualComponents)
         }
     }
 
-    fun find(value: T): MutableSet<T>? {
-        return data.find { item -> item.contains(value) }
+    fun find(value: T): MutableSet<Node<T>>? {
+        return components.find { item -> item.contains(value.asNode()) }
     }
 
     fun isConnected(vertex1: T, vertex2: T): Boolean {
         val (component1, component2) = find(vertex1) to find(vertex2)
         return component1 != null && component1 == component2
     }
+
+    data class Node<T>(
+        val value: T,
+        var visited: Boolean = false
+    )
+
+    private fun <T> T.asNode(): Node<T> {
+        return Node(value = this)
+    }
+}
+
+fun main() {
+    val graph = DynamicGraph<Int>()
+    graph.addEdge(1, 2)
+    graph.addEdge(2, 3)
+    graph.addEdge(4, 5)
+
+    println(graph.isConnected(1, 2))
+    println(graph.isConnected(1, 3))
+    println(graph.isConnected(1, 4))
 }
